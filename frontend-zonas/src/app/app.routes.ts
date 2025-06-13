e0ae4514ffbdfecb3d6c1e9e5ca5d32fb3b9f3ad
PIPICBA <lichimedina05@gmail.com>
2025-06-12 23:40:45 -0300

    Facilito
    Creación de la base de los componentes de login y register.

---
+++ frontend-zonas/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+];
